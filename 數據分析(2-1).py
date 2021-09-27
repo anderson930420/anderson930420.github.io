6b8f31c77d17fdc452e855e35510bc6b290622e7
Anderson930420 <73323931+Anderson930420@users.noreply.github.com>
2021-09-28 02:50:21 +0800

    Update 數據分析(2-1).py

--- 數據分析(2-1).py
+++ 數據分析(2-1).py
@@ -1,34 +1,35 @@
+#輸入第1組資料
 stats1 = list()
 stats1.append(input("請輸入10筆資料\n"))
 while True:
     b = input()
     if len(stats1) in range(10):
-        stats1.append(b)
+        stats1.append(b)                #將輸入的資料加入陣列stats1中
     else:
         break
 print(stats1)
 
 sum = 0
 for i in range(10):
-     sum = sum + int(stats1[i])
-     ave = sum/10
+     sum = sum + int(stats1[i])         #算出總和
+     ave = sum/10                       #算出平均
 print(sum)
 print(ave)
 
-
+#輸入第2組資料
 stats2 = list()
 stats2.append(input("\n請輸入10筆資料\n"))
 while True:
     b = input()
-    if len(stats2) in range(10):
-        stats2.append(b)
+    if len(stats2) in range(10):        
+        stats2.append(b)                #將輸入的資料加入陣列stats2中                         
     else:
         break
 print(stats2)
 
 sum = 0
 for i in range(10):
-     sum = sum + int(stats2[i])
-     ave = sum/10
+     sum = sum + int(stats2[i])         #算出總和                        
+     ave = sum/10                       #算出平均
 print(sum)
 print(ave)
