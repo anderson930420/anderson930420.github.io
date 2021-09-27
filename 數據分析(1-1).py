0de9be5f3d833ffba0ca47ecac04ea6460b5c17c
Anderson930420 <73323931+Anderson930420@users.noreply.github.com>
2021-09-28 02:45:48 +0800

    Update 數據分析(1-1).py

--- 數據分析(1-1).py
+++ 數據分析(1-1).py
@@ -1,3 +1,4 @@
+#輸入10筆資料
 A = input("請輸入A1： ")
 B = input("請輸入B1： ")
 C = input("請輸入C1： ")
@@ -8,8 +9,9 @@ G = input("請輸入G1： ")
 H = input("請輸入H1： ")
 I = input("請輸入I1： ")
 J = input("請輸入J1： ")
-
+#將10筆資料存入stats陣列
 stats = [int(A),int(B),int(C),int(D),int(E),int(F),int(G),int(H),int(I),int(J)]
+#算出總合與平均值
 sum = 0
 for i in range(10):
      sum = sum + stats[i]
